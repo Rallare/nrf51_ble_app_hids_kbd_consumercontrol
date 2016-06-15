380347346770cbd0dd86cfc6f58d7206d71121ea
Rallare <alseth@gmail.com>
2016-06-15 13:30:46 +0200

    added template support for nrf52832

---
+++ pca10040/s132/arm5_no_packs/_build/app_trace.d
@@ -0,0 +1,5 @@
+.\_build\app_trace.o: ..\..\..\..\..\..\components\libraries\trace\app_trace.c
+.\_build\app_trace.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdio.h
+.\_build\app_trace.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\app_trace.o: C:\Keil\ARM\ARMCC\Bin\..\include\string.h
+.\_build\app_trace.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdarg.h
